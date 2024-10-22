838c70152ceabc47ebd03a6797eb46c8152cb525
botniedata <botniedata@gmail.com>
2024-10-22 10:20:58 +0800

    adds normalized change of popularity

--- 3_data_manipulation_with_dplyr/4_babynames_data/babynames_3viznormalize.R
+++ 3_data_manipulation_with_dplyr/4_babynames_data/babynames_3viznormalize.R
@@ -15,31 +15,31 @@ rm(list = ls())
 babynames <- read_csv("F:\\R\\dataset\\babynames.csv")
 
 ### Code ----
-  # Calculate the fraction of people born each year with the same name
+# Calculate the fraction of people born each year with the same name
 babynames %>%
   group_by(year) %>% # group it by year
   mutate(year_total = sum(number)) %>% # calculate sum of number per year
   ungroup() %>% # remove grouping applied in the data frame
   mutate(fraction = number / year_total) %>%
-
+  
   # Find the year each name is most common
   group_by(name) %>% # group it by name
   slice_max(fraction, n = 1) # slice on it's maximum result on fraction
 
 ### Code ----
-  # Adding the total and maximum for each name
+# Adding the total and maximum for each name
 babynames %>%
   group_by(name) %>%
   
   # name_total: the sum of the number of babies born with the name in the entire dataset.
   mutate(name_total = sum(number),
-  # name_max: the maximum of babies born with that name in a year.       
+         # name_max: the maximum of babies born with that name in a year.       
          name_max = max(number)) %>%
   # ungroup the table
   ungroup() %>%
   # create a fraction of of name_max in a whole dataset
   mutate(fraction_max = number / name_max)
 
-    
+
 
 
