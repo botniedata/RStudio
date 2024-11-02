eb07b29c57a7669712816cfd05649beccb93ec47
botniedata <botniedata@gmail.com>
2024-11-02 09:55:23 +0800

    adds right join first part

---
+++ 4_joining_data_with_dplyr/1_joining_data_dplyr/2_left_right_joins/rightjoin_1.R
@@ -0,0 +1,34 @@
+# Joining Data with dplyr ----
+## right_join() verb ----
+# right_join() function from the dplyr package is used to perform a right outer join. This type of join keeps all rows from the right data frame, even if there are no matching rows in the left da
+
+### Recall about Joining tables ----
+# inner_join() : keep only observations that match perfectly between tables
+# left_join() : keep all observation in the first table, including matches observations in the second table
+# right_join() : keep all observation in the second table, including matches observations in the first table.
+
+# Load packages ----
+library(dplyr)
+library(tidyverse)
+library(readr)
+
+# Restart Environment ----
+rm(list = ls())
+
+# Load CSV --
+parts <- read_csv("F:\\R\\dataset\\parts.csv")
+part_categories <- read_csv("F:\\R\\dataset\\part_categories.csv")
+
+# Code ----
+  # Count the part_cat_id
+  x <- parts %>%
+    count(part_cat_id, sort = TRUE) %>%
+
+  # Right join part_categories
+  right_join(part_categories, by = c("part_cat_id" = "id")) %>%
+  
+  # Filter for NA
+  filter(is.na(n))
+
+  print(x)
+
