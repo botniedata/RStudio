c2aae04a8a1155b1316599e9f88e8c67234ff0cd
botniedata <botniedata@gmail.com>
2024-11-04 14:37:11 +0800

    join questions, answers, question_tags and tags using inner join

---
+++ 4_joining_data_with_dplyr/joining_data_dplyr/4_case_study_stackoverflow/case_study_6joinquestionsanswerstags.R
@@ -0,0 +1,31 @@
+# Case Study: Joins on Stack Overflow Data ----
+
+# Load packages ----
+library(dplyr)
+library(tidyverse)
+library(readr)
+
+# Restart Environment ----
+rm(list = ls())
+
+# Load CSV ----
+questions <- read_csv("F:\\R\\dataset\\stack over flow\\questions.csv")
+answers <- read_csv("F:\\R\\dataset\\stack over flow\\answers.csv")
+question_tags <- read_csv("F:\\R\\dataset\\stack over flow\\question_tags.csv")
+tags <- read_csv("F:\\R\\dataset\\stack over flow\\tags.csv")
+
+answer_counts <- answers %>%
+  count(question_id, sort = TRUE)
+
+question_answer_counts <- questions %>%
+  left_join(answer_counts, by = c("id" = "question_id")) %>%
+  replace_na(list(n = 0))
+
+# Code ----
+question_answer_counts %>%
+  # Join the question_tags tables
+  inner_join(question_tags, by = c("id" = "question_id")) %>%
+  # Join the tags table
+  inner_join(tags, by = c("tag_id" = "id"))
+
+
