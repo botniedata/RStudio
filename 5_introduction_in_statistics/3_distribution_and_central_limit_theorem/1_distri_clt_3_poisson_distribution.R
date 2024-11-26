030da6768d4f1c310ac9ff98ea9d92005e07e7ac
botniedata <botniedata@gmail.com>
2024-11-26 13:53:47 +0800

    adds poisson distri exercises

--- 5_introduction_in_statistics/3_distribution_and_central_limit_theorem/1_distri_clt_3_poisson_distribution.R
+++ 5_introduction_in_statistics/3_distribution_and_central_limit_theorem/1_distri_clt_3_poisson_distribution.R
@@ -1,72 +1,48 @@
 # Introduction to Statistics in R ----
 
-## Normal Distribution ----
-# Shape is commonly referred to as a "bell curve"
-# Properties of a normal distribution, 
-# • it's symmetrical (left side is mirror image of the right)
-# • the area beneath the curve is equal to 1
-# • probability never hits 0 (zero) - 0.006% of its area is contained beyond the edges of this graph
+## poisson distribution ----
+# events appear to happen at a certain rate, but completely at random
+# example:
+# number of animals adopted from an animal shelter per week
+# number of people arriving at a restaurant per hour
+# number of earthquakes in California per year
 
-# Normal distribution (68-95-99.7 rule): 
-# 68% of the area falls within 1 standard deviation
-# 95% of the area falls within 2 standard deviation
-# 99.7% of the area falls within 3 standard deviation
+# probability of some # of events occurring over a fixed period of time
+# example:
+# probability of >= 5 animals adopted from an animal shelter per week
+# probability of 12 people arriving at a restaurant per hour
+# probability of < 20 earthquakes in California per year
+  
+# lambda (λ) - average number of events per time interval
+# • average number of adoptions per week = 8 (expected value of the distribution)
 
-# Load packages ---- 
-library(dplyr)
-library(readr)
-library(ggplot2)
+# Code:
+# if the average number of adoption per week is 8, what is P(# adoption in a week = 5)?
+# dpois(5, lambda = 8)
+
+# # if the average number of adoption per week is 8, what is P(# adoption in a week <= 5)?
+# ppois(5, lambda = 8)
+
+# # if the average number of adoption per week is 8, what is P(# adoption in a week > 5)?
+# ppois(5, lambda = 8, lower.tail = FALSE)
+
+# sampling from a Poisson distribution
+# in 10 weeks with average number of 8
+# rpois(10, lambda = 8)
 
 # Restart Environment ----
 rm(list = ls())
 
-# Load data set ----
-# Data set
-data <- read_csv("F:\\R\\dataset\\all_deals.csv")
-data2 <- read_csv("F:\\R\\dataset\\amir_deals.csv")
-
-# Reconstruction of data set data ---
-product <- as.integer(data$product)
-num_users <- as.double(data$num_users)
-all_deals <- data.frame(product, num_users)
-
-product <- data2$product
-product <- factor(product,
-                  levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14),
-                  labels = c("Product A", "Product B", "Product C", "Product D",
-                             "Product E", "Product F", "Product G", "Product H",
-                             "Product I", "Product J", "Product K", "Product L",
-                             "Product N",  "Product P"))
-client <- data2$client
-client <- factor(client,
-                 levels = c(1, 2, 3),
-                 labels = c("Curent", "New", "Past"))
-
-status <- data2$status
-status <- factor(status,
-                 levels = c(1, 2),
-                 labels = c("Lost", "Won"))
-
-amount <- data2$amount
-num_users <- data2$num_users
-
-# Save as data frame named amir_deals
-amir_deals <- data.frame(product, client, status, amount, num_users)
-
-
 # Code ----
-# Set seed to 321
-set.seed(321)
+# What's the probability that Amir responds to 5 leads in a day, given that he responds to an average of 4?
+dpois(5, lambda = 4)
 
-# Take 30 samples of 20 values of num_users, take mean of each sample
-sample_means <- replicate(30, sample(all_deals$num_users, 20) %>% mean())
+# Amir's coworker responds to an average of 5.5 leads per day. What is the probability that she answers 5 leads in a day?
+dpois(5, lambda = 5.5)
 
-# Calculate mean of sample_means
-mean(sample_means)
+# What's the probability that Amir responds to 2 or fewer leads in a day?
+ppois(2, lambda = 4)
 
-# Calculate mean of num_users in amir_deals
-mean(amir_deals$num_users)
-
-
-
+# What's the probability that Amir responds to more than 10 leads in a day?
+ppois(10, lambda = 4, lower.tail = FALSE)
 
