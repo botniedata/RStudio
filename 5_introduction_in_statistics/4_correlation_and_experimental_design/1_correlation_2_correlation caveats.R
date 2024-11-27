bc8fe29de2900299fdcd3f9d02c6334e490fa444
botniedata <botniedata@gmail.com>
2024-11-27 09:23:30 +0800

    adds correlation example

--- 5_introduction_in_statistics/4_correlation_and_experimental_design/1_correlation_2_correlation caveats.R
+++ 5_introduction_in_statistics/4_correlation_and_experimental_design/1_correlation_2_correlation caveats.R
@@ -36,12 +36,15 @@ world_happiness <- data.frame(country, social_support, freedom, corruption,
                               generosity, gdp_per_cap, life_exp, happiness_score)
 
 # Code ----
-# Create a scatter plot of happiness_score vs. life_exp
-ggplot(world_happiness, aes(x = life_exp, y = happiness_score)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
+# Create a scatter plot of gdp_per_cap vs. life_exp
+ggplot(world_happiness, aes(x = gdp_per_cap, y = life_exp)) +
+  geom_point()
 
-# Calculate the correlation 
-cor(world_happiness$life_exp, world_happiness$happiness_score)
+# Correlation between gdp_per_cap and life_exp
+cor(world_happiness$gdp_per_cap, world_happiness$life_exp)
+
+# Note:
+# The correlation between GDP per capita and life expectancy is 0.7. Why is correlation not the best way to measure the relationship between the two variables?
+# Answer: Correlation only measures linear relationships.
 
 
